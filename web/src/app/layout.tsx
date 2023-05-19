a44fcf5f55a4d5825171ad4d4d397e4da2e8a70f
unknown <pedroalmeida.programador@gmail.com>
2023-05-19 14:42:16 -0300

    feature-1

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,32 @@
+import { ReactNode } from 'react'
+import './globals.css'
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamjuree,
+} from 'next/font/google'
+
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+
+const baiJamjuree = BaiJamjuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-bai-jamjuree',
+})
+
+export const metadata = {
+  title: 'NLW Spacetime',
+  description:
+    'Uma capsula do tempo construida com React, NextJS, tailwind e Typescript',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
