ada0a6fd6b1b2838587b06243744ddb609808b45
Pedro Almeida <pedroalmeida.programador@gmail.com>
2023-05-22 22:00:33 -0300

    Feature 4/5
    NLW Spacetime

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,33 @@
+import 'dotenv/config'
+
+import multipart from '@fastify/multipart'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'path'
 
 const app = fastify()
 
 app.register(cors, {
   origin: true,
 })
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+app.register(multipart)
+
+app.register(jwt, {
+  secret: 'spacetime', // Bater a cabeça no teclado
+})
+
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app
   .listen({
